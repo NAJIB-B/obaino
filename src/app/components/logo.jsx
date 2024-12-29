15d1227041a6ef18cb8304655bf846e335392bb9
Godwin Nwafor <nwfrgodwin@gmail.com>
2024-12-29 10:32:02 +0100

    update

--- src/app/components/logo.jsx
+++ src/app/components/logo.jsx
@@ -23,7 +23,7 @@ export default function Logo() {
                     className="sm:hidden block"
                 ></Image>
                 <Link href={'https://calendly.com/your-username'} target="_blank">
-                    <div className="absolute flex flex-row items-center rounded-[80px] border border-darkGray sm:py-2 max-[400px]:px-1 px-2 sm:px-[12px] right-4 max-[500px]:right-1 gap-1 sm:gap-3 text-[16px] max-[500px]:text-[9px]">
+                    <div className="absolute flex flex-row items-center rounded-[80px] border border-darkGray sm:py-2 max-[400px]:px-1 px-2 sm:px-[12px] right-4 max-[500px]:right-1 gap-1 sm:gap-3 text-[16px] max-[500px]:text-[13px]">
                         <FiPhone
                             icon={faEnvelope}
                             width={20}
