e1a52aa916400987a9e57dec1d61cfb6213eaef4
NAJIB-B <najibajibasu@gmail.com>
2024-12-30 20:01:59 +0100

    Added service card width

--- src/app/components/services.jsx
+++ src/app/components/services.jsx
@@ -18,7 +18,7 @@ const Services = ({ services }) => {
       </div>
       <div className=" md:inline-flex gap-5 my-6 overflow-auto service-scrollbar pb-6 ">
         {services.map((service, index) => (
-          <div key={index} className="flex flex-col p-4 md:max-w-[410px] flex-shrink-0 border border-gray2  hover:border-black2 
+          <div key={index} className="flex flex-col p-4 md:max-w-[430px] flex-shrink-0 border border-gray2  hover:border-black2 
           hover:bg-black2 hover:text-white rounded-[7px] max-[768px]:my-8">
             <h4 className="font-bold text-[22px] my-4">{service?.title}</h4>
             <p className="text-[14px] my-3">
